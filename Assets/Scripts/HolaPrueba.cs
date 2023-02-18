e7283fbb37e946ef58c1136c02fb7707e9451f2f
P-aby <108104087+P-aby@users.noreply.github.com>
2023-02-17 18:02:37 -0600

    Proyecto de Unity
    Primer commit para subir proyecto de unity

---
+++ Assets/Scripts/HolaPrueba.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HolaPrueba : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hola a todos");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
