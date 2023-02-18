060b848e11672fd65cb787efcb6073452e8caa8f
P-aby <108104087+P-aby@users.noreply.github.com>
2023-02-17 18:27:26 -0600

    Script _Monedas_V01.0.1
    Se crea el script para contar monedas

---
+++ Assets/Scripts/ContadorMonedas.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ContadorMonedas : MonoBehaviour
+{
+    public int monedas;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void Monedas()
+    {
+        monedas++;
+    }
+}
